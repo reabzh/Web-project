efc9002402ce1011481688fc230e991f27929cc7
Bushra  abdulaziz <114013251+asdfzxcvl@users.noreply.github.com>
2023-02-09 20:31:49 +0300

    Update post.js

--- post.js
+++ post.js
@@ -1,3 +1,33 @@
+var http = require('http');
+var postHTML =
+'<html><head> <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"> <link rel="stylesheet" href="logstle.css"> </head>' +
+'<body><div><p><div class="top-header"><span >Have an account?</span>  <h3 class="lg">LogIn</h3></p>' +
+'<form action="/" method="POST"><div class="input-field">' +
+'<input type="text" class="input" placeholder="Username" id="firstname" name="firstname" required><i class="bx bx-user"></i></div>' +
+' <div class="input-field"><input type="password" class="input" placeholder="Password" id="password" name="password" required>   <i class="bx bx-lock-alt"></i></div>' +
+'<div class="input-field"><input type="submit" class="submit" value="Login"></div>' +
+'<div class="bottom"><div class="left"> <input type="checkbox" id="check"> <label for="check">Remember Me</label></div>' +
+'<div class="right"> <label for="check"><a href="#">Forget your password?</a></label></div>' +
+'</div></div></div></form></body></html>';  
+var qs = require('querystring')
+http.createServer(function (req, res) {
+    res.end(postHTML);
+    if (req.method == 'POST') {
+        var body = "";
+        req.on('data', function (data) {
+            body = body + data;
+        });
+        req.on('end', function () {
+            var post = qs.parse(body);
+            console.log(post['firstname']);
+            console.log(post['password']);
+        });
+    }
+}).listen(3000);
+console.log("server running");
+
+
+
 /*var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
